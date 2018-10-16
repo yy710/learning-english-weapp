4bb9f7c60bab2e5c31d10c3abd990b47b4f617cd
yy710 <171254868@qq.com>
2018-10-16 17:49:27 +0800

    ent

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,8 +1,13 @@
 // pages/classic/classic.js
-import {ClassicModel} from '../../models/classic.js'
-import { LikeModel } from '../../models/like.js'
+import {
+  ClassicModel
+} from '../../models/classic.js'
+import {
+  LikeModel
+} from '../../models/like.js'
 let classicModel = new ClassicModel()
 let likeModel = new LikeModel()
+const baseUrl = "https://www.all2key.cn/"
 
 Page({
 
@@ -10,46 +15,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-    classic:null,
-    latest:true,
-    first:false,
-    like:false,
-    count:0
+    classic: {
+      type: 200,
+      image: baseUrl + 'images/001.png',
+      url: baseUrl + 'audio/01intoWoods.mp3',
+      title: 'Dinosaurs Before Dark'
+    },
+    latest: true,
+    first: false,
+    like: false,
+    count: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    classicModel.getLatest((data)=>{
+  onLoad: function(options) {
+    classicModel.getLatest((data) => {
       this._getLikeStatus(data.id, data.type)
       this.setData({
         //classic:data
-        classic: {type: 300}
+        classic: {
+          type: 300
+        }
       })
     })
   },
 
-  onPrevious:function(event){
+  onPrevious: function(event) {
     let index = this.data.classic.index
-    classicModel.getPrevious(index, (data)=>{
-      if(data){
-        this._getLikeStatus(data.id, data.type)
-        this.setData({
-          classic:data,
-          latest: classicModel.isLatest(data.index),
-          first: classicModel.isFirst(data.index)
-        })
-      }
-      else{
-        console.log('not more classic')
-      }
-    })
-  },
-
-  onNext:function(event){
-    let index = this.data.classic.index
-    classicModel.getNext(index, (data)=>{
+    classicModel.getPrevious(index, (data) => {
       if (data) {
         this._getLikeStatus(data.id, data.type)
         this.setData({
@@ -57,28 +52,43 @@ Page({
           latest: classicModel.isLatest(data.index),
           first: classicModel.isFirst(data.index)
         })
-      }
-      else {
+      } else {
         console.log('not more classic')
       }
     })
   },
 
-  onLike:function(event){
+  onNext: function(event) {
+    let index = this.data.classic.index
+    classicModel.getNext(index, (data) => {
+      if (data) {
+        this._getLikeStatus(data.id, data.type)
+        this.setData({
+          classic: data,
+          latest: classicModel.isLatest(data.index),
+          first: classicModel.isFirst(data.index)
+        })
+      } else {
+        console.log('not more classic')
+      }
+    })
+  },
+
+  onLike: function(event) {
     let like_or_cancel = event.detail.behavior
     likeModel.like(like_or_cancel, this.data.classic.id, this.data.classic.type)
   },
 
-  _getLikeStatus:function(cid, type){
-    likeModel.getClassicLikeStatus(cid, type, (data)=>{
+  _getLikeStatus: function(cid, type) {
+    likeModel.getClassicLikeStatus(cid, type, (data) => {
       this.setData({
-        like:data.like_status,
-        count:data.fav_nums
+        like: data.like_status,
+        count: data.fav_nums
       })
     })
   },
 
-  onShareAppMessage(){
+  onShareAppMessage() {
 
   }
 })
